dc477ac6c587c3014c8deedabb8529ae43f7f11f
Michael V Dormand <michael.dormand@ibm.com>
2025-01-22 23:23:35 +0000

    Throw Error if no Markdown

--- backend/src/routes/render.js
+++ backend/src/routes/render.js
@@ -6,6 +6,10 @@ const router = express.Router();
 router.post("/", (req, res) => {
     const { markdown } = req.body;
 
+    if (!markdown) {
+        return res.status(400).json({error: "Markdown is required. Please send with { \"markdown\": {markdown} }"});
+    }
+
     const html = marked.parse(markdown);
     res.json({html});
 
