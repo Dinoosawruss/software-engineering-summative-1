bf160970bb628f090706d5719336d90dd704f621
Michael V Dormand <michael.dormand@ibm.com>
2025-01-22 23:04:43 +0000

    Make Initial Test Pass

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+const renderRoute = require("./routes/render");
+
 dotenv.config();
 
 const app = express();
@@ -11,5 +13,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(morgan("dev"))
 app.use(express.json());
+
+app.use("/render", renderRoute);
 
 app.listen(PORT, () => console.log(`Server us running on http://localhost:${PORT}`));
