b5366486500865f261dc64b20d5d0a3658f97238
Michael V Dormand <michael.dormand@ibm.com>
2025-01-23 21:41:30 +0000

    Backend Fonts Endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+const fontsRoute = require("./routes/fonts");
 const renderRoute = require("./routes/render");
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(morgan("dev"))
 app.use(express.json());
 
+app.use("/fonts", fontsRoute);
 app.use("/render", renderRoute);
 
 app.listen(PORT, () => console.log(`Server us running on http://localhost:${PORT}`));
---
+++ backend/src/routes/fonts.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const fonts = [
+        { name: "Courier Prime", value: "\"Courier Prime\", monospace" },
+        { name: "Arial", value: "Arial" },
+        { name: "Verdana", value: "Verdana" },
+        { name: "Georgia", value: "Georgia" },
+    ];
+    res.json(fonts);
+});
+
+module.exports = router;
