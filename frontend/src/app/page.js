d718a58ff79c74b431553ea6271ebfb28d04da2e
Michael V Dormand <michael.dormand@ibm.com>
2025-01-23 10:42:10 +0000

    Add Markdown Textarea

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,17 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
+import React from 'react';
 
 export default function Home() {
-    return (
-      <div>
-        <h1><strong><u>GoodMark</u></strong></h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1><strong><u>GoodMark</u></strong></h1>
+      <textarea
+        class="textzone"
+        rows="10"
+        cols="50"
+        placeholder="Enter markdown here"
+      />
+    </div>
+  );
+}
