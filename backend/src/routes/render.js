853060b6d2e71d79aad96dcbdbe8faf243e6befc
Michael V Dormand <michael.dormand@ibm.com>
2025-01-24 19:48:59 +0000

    Add Caching

--- backend/src/routes/render.js
+++ backend/src/routes/render.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const marked = require("marked");
+const NodeCache = require("node-cache");
 
 const router = express.Router();
+const cache = new NodeCache();
 
 router.post("/", async (req, res) => {
     const { markdown } = req.body;
@@ -10,8 +12,16 @@ router.post("/", async (req, res) => {
         return res.status(400).json({error: "Markdown is required. Please send with { \"markdown\": {markdown} }"});
     }
 
+    const cacheKey = `markdown:${markdown}`
+    const cachedHtml = cache.get(cacheKey);
+
+    if (cachedHtml) {
+        return res.json({ html: cachedHtml });
+    }
+
     try {
         const html = marked.parse(markdown);
+        cache.set(cacheKey, html);
         res.json({html});
     } catch (err) {
         res.status(500).json({error: `Error rendering Markdown: ${err.message}`})
