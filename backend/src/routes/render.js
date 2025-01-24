a62fee61544d085ad0b52eb3e49cd0583d44c90f
Michael V Dormand <michael.dormand@ibm.com>
2025-01-24 19:50:53 +0000

    Improve Logging

--- backend/src/routes/render.js
+++ backend/src/routes/render.js
@@ -2,6 +2,12 @@ const express = require("express");
 const marked = require("marked");
 const NodeCache = require("node-cache");
 const sanitizeHtml = require("sanitize-html");
+const winston = require("winston");
+
+const logger = winston.createLogger({
+    level: "error",
+    transports: [new winston.transports.Console()],
+});
 
 const router = express.Router();
 const cache = new NodeCache();
@@ -9,7 +15,7 @@ const cache = new NodeCache();
 router.post("/", async (req, res) => {
     const { markdown } = req.body;
 
-    if (!markdown) {
+    if (!markdown.trim()) {
         return res.status(400).json({error: "Markdown is required. Please send with { \"markdown\": {markdown} }"});
     }
 
@@ -23,10 +29,13 @@ router.post("/", async (req, res) => {
     try {
         const rawHtml = marked.parse(markdown);
         const sanitizedHtml = sanitizeHtml(rawHtml);
+
         cache.set(cacheKey, sanitizedHtml);
+
         res.json({ html: sanitizeHtml });
     } catch (err) {
-        res.status(500).json({error: `Error rendering Markdown: ${err.message}`})
+        logger.error(`Error rendereing Markdown: ${err.stack}`);
+        res.status(500).json({error: `Error rendering Markdown: ${err.message}`});
     }
 })
 
