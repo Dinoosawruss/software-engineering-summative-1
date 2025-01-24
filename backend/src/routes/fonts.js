44d8a23bde3fad5092ec269335a186c8cd61713a
Michael V Dormand <michael.dormand@ibm.com>
2025-01-24 19:44:59 +0000

    Make Fonts Async

--- backend/src/routes/fonts.js
+++ backend/src/routes/fonts.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     const fonts = [
         { name: "Courier Prime", value: "\"Courier Prime\", monospace" },
         { name: "Arial", value: "Arial" },
