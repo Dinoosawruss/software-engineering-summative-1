f25799f0b2d7b8cb6b4fc1cb9729ae77b8f94751
Michael V Dormand <michael.dormand@ibm.com>
2025-01-24 20:46:39 +0000

    Temporarily Remove Rate Limit for Performance Test

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const rateLimit = require("express-rate-limit");
+// const rateLimit = require("express-rate-limit");
 
 const fontsRoute = require("./routes/fonts");
 const renderRoute = require("./routes/render");
 
-const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 500, // Rate limit is 500 requests per minute
-    message: "Too many requests",
-})
+// const limiter = rateLimit({
+//     windowMs: 1 * 60 * 1000,
+//     max: 500, // Rate limit is 500 requests per minute
+//     message: "Too many requests",
+// })
 
 dotenv.config();
 
@@ -23,7 +23,7 @@ app.use(morgan("dev"))
 app.use(express.json());
 
 app.use("/fonts", fontsRoute);
-app.use("/render", limiter);
+// app.use("/render", limiter);
 app.use("/render", renderRoute);
 
 app.listen(PORT, () => console.log(`Server us running on http://localhost:${PORT}`));
